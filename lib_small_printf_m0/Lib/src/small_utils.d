6c91c91284407be1e8eb1a194fdc565fff7d8e08
ben_meng <ben_meng@compalcomm.com>
2015-01-22 14:58:05 +0800

    remove useless files

--- lib_small_printf_m0/Lib/src/small_utils.d
+++
@@ -1,4 +0,0 @@
-src/small_utils.d: ../src/small_utils.c \
- /media/BenStorage/Desktop/MyPrj/sample/lib_small_printf_m0/inc/small_utils.h
-
-/media/BenStorage/Desktop/MyPrj/sample/lib_small_printf_m0/inc/small_utils.h:
